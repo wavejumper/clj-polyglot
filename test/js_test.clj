b5ef58d2282ce51095e36e22a70364da0b3691b7
Thomas Crowley <crowley@kibu.com.au>
2020-05-25 22:12:50 +1000

    Unit tests testing recursive serialize-arg

--- test/js_test.clj
+++ test/js_test.clj
@@ -7,6 +7,9 @@
   (let [tonal-src (slurp "https://cdn.jsdelivr.net/npm/@tonaljs/tonal/browser/tonal.min.js")
         ctx       (poly.js/js-ctx tonal-src)
         Tonal     (poly.js/require ctx "Tonal")
-        api       (poly.js/import Tonal :Note [:midi :freq])
-        f         #(poly/eval api :freq %)]
-    (is (= 440 (f "A4")))))
+        api       (poly.js/import Tonal :Note [:midi :freq :names])
+        freq      #(poly/eval api :freq %)
+        names     #(poly/eval api :names %)]
+    (is (= 440 (freq "A4")))
+    ;; test recusion
+    (is (= ["F##"] (names ["fx" [""] {"a" {"b" :c}}])))))
